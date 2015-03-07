8bdcb7284a278ded3e7b45e4558a989c5357e1e8
Nat Wilson <njwilson23@gmail.com>
2015-03-06 21:26:31 -0500

    add tool for searching directories for PDFs

--- peer-pdf/peer-pdf.go
+++ peer-pdf/peer-pdf.go
@@ -1,17 +1,56 @@
 package main
 
 import (
-	"flag"
+	//"flag"
 	"fmt"
 	"github.com/njwilson23/peer2/config"
-	"gopkg.in/yaml.v2"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 // USAGE:
 // peer-pdf [query] [-r] [-p] [-o N]
 
+// Walk a root, sending file matches to out channel
+func SearchRoot(root string, searchstr string, out *[]string) {
+	filepath.Walk(root, func(fnm string, info os.FileInfo, err error) error {
+		if err != nil {
+			fmt.Println("ERROR:", fnm, err)
+			return filepath.SkipDir
+		}
+		if strings.ToLower(filepath.Ext(fnm)) == ".pdf" {
+			if strings.Contains(strings.ToLower(fnm), searchstr) {
+				*out = append(*out, fnm)
+			}
+		}
+		return nil
+	})
+}
+
 func main() {
 
+	var searchstr string
 	config := config.ParseConfig(".peer.yaml")
+	if len(os.Args) != 1 {
+		searchstr = os.Args[1]
+	} else {
+		fmt.Println("ERROR: must provide at least one search query")
+		os.Exit(1)
+	}
+	if len(config.SearchRoots) == 0 {
+		fmt.Println("ERROR: list of search roots is empty")
+		os.Exit(1)
+	}
 
+	// Search each directory root
+	results := make([]string, 0)
+	for _, root := range config.SearchRoots {
+		SearchRoot(root, searchstr, &results)
+	}
+
+	// Print matches
+	for i, match := range results {
+		fmt.Println(i+1, filepath.Base(match))
+	}
 }
