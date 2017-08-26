78e06ccc78b3fb5f69de234f41e09a3285b8029f
Nat Wilson <njwilson23@gmail.com>
2017-08-26 12:51:49 -0700

    Catch case when no argument supplied

--- peer.go
+++ peer.go
@@ -8,7 +8,10 @@ import (
 
 func main() {
 	var cmd *exec.Cmd
-	if os.Args[1] == "bib" {
+	if len(os.Args) == 1 {
+		fmt.Println("Usage: peer pdf|bib args...")
+		os.Exit(0)
+	} else if os.Args[1] == "bib" {
 		cmd = exec.Command("peer-bib", os.Args[2:]...)
 	} else if os.Args[1] == "pdf" {
 		cmd = exec.Command("peer-pdf", os.Args[2:]...)
